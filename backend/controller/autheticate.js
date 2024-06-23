7a7e067a07460f4694051cd1d2a77389460442b2
Amisha Sahu <amishasahu328@gmail.com>
2024-06-23 19:56:59 +0530

    updated reset-password url

--- backend/controller/autheticate.js
+++ backend/controller/autheticate.js
@@ -151,7 +151,7 @@ exports.requestPasswordReset = async (req, res) => {
     subject: "Password Reset",
     text: `You are receiving this because you have requested the reset of the password for your account.\n\n
            Please click on the following link, or paste this into your browser to complete the process:\n
-           http://localhost:5173/reset-password/${user._id}/${token}
+           ${process.env.FRONTEND_URL}/reset-password/${user._id}/${token}
            If you did not request this, please ignore this email and your password will remain unchanged.\n`,
   };
 
