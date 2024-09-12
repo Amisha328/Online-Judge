6ad8578cc92dff8929c7a3efcedeb2b9bf5b584a
Amisha Sahu <amishasahu328@gmail.com>
2024-09-12 20:03:56 +0530

    Added new features

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,43 +2,6 @@ const User = require('../models/User')
 const Competition = require('../models/Competition');
 const Problem = require('../models/Problems');
 
-/*
-exports.getUserProfile = async (req, res) => {
-          try {
-                    const user = await User.findById(req.params.userId);
-                    if (!user) {
-                      return res.status(404).json({ error: 'User not found' });
-                    }
-                
-                    const competitions = await Competition.find({ 'submissions.user_id': req.params.userId });
-                    const problemsSolved = [];
-                
-                    for (const competition of competitions) {
-                      for (const submission of competition.submissions) {
-                        if (submission.user_id.toString() === req.params.userId && submission.verdict === 'Accepted') {
-                          const problem = await Problem.findById(submission.problem_id);
-                          if (problem) {
-                            problemsSolved.push({
-                              problemDetails: {
-                                title: problem.title,
-                                tags: problem.tags,
-                                difficulty: problem.difficulty,
-                              },
-                              submissionTime: submission.submissionTime,
-                            });
-                          }
-                        }
-                      }
-                    }
-                    // console.log(user);
-                    // console.log(problemsSolved);
-                    res.status(200).json({ user , problemsSolved });
-          } catch (error) {
-            res.status(500).json({ error: 'Internal server error' });
-          }
-        };
-*/
-
 exports.getUserProfile = async (req, res) => {
   try {
     // console.log("Fetching user with ID:", req.params.userId);
@@ -74,15 +37,12 @@ exports.getUserProfile = async (req, res) => {
       }
     }
 
-    // console.log("Competition problems solved:", competitionProblemsSolved);
-
     // Fetch practice problems solved
     const practiceProblems = await Problem.find({
       'submissions.userId': req.params.userId,
       'submissions.verdict': 'Accepted'
     });
 
-    // console.log("Practice submissions found:", practiceProblems);
 
     let countProblemsSolved = 0;
 
@@ -93,7 +53,6 @@ exports.getUserProfile = async (req, res) => {
       }
     }
 
-    // console.log("Practice problems solved:", countProblemsSolved);
 
     res.status(200).json({ user, competitionProblemsSolved, countProblemsSolved });
   } catch (error) {
@@ -122,7 +81,6 @@ exports.updateUser = async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    // console.log('Updated Profile:', updatedProfile);
 
     res.status(200).json({ status: true, message: 'User updated successfully!', user: updatedProfile });
   } catch (error) {
